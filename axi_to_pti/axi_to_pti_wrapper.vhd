10585d3a68a1a51ada990b0a90e32add64eb896b
Mathias Sandner <mathias.sandner@lauterbach.com>
2024-04-15 10:38:52 +0200

    Rename ports for automatic assignment to AXI4 slave interface

--- axi_to_pti/axi_to_pti_wrapper.vhd
+++ axi_to_pti/axi_to_pti_wrapper.vhd
@@ -8,39 +8,39 @@ entity axi_to_pti_wrapper is
 		iRstN:      in  std_logic;
 		iClk:       in  std_logic;
 
-		oAwready:   out std_logic;
-		iAwvalid:   in  std_logic;
-		iAwid:      in  std_logic_vector( 8 downto 0);
-		iAwaddr:    in  std_logic_vector(38 downto 0);
-		iAwlen:     in  std_logic_vector( 7 downto 0);
-		iAwburst:   in  std_logic_vector( 1 downto 0);
-		iAwsize:    in  std_logic_vector( 1 downto 0);
+		AWREADY:    out std_logic;
+		AWVALID:    in  std_logic;
+		AWID:       in  std_logic_vector( 8 downto 0);
+		AWADDR:     in  std_logic_vector(38 downto 0);
+		AWLEN:      in  std_logic_vector( 7 downto 0);
+		AWBURST:    in  std_logic_vector( 1 downto 0);
+		AWSIZE:     in  std_logic_vector( 1 downto 0);
 
-		oWready:    out std_logic;
-		iWvalid:    in  std_logic;
-		iWlast:     in  std_logic;
-		iWstrb:     in  std_logic_vector( 7 downto 0);
-		iWdata:     in  std_logic_vector(63 downto 0);
+		WREADY:     out std_logic;
+		WVALID:     in  std_logic;
+		WLAST:      in  std_logic;
+		WSTRB:      in  std_logic_vector( 7 downto 0);
+		WDATA:      in  std_logic_vector(63 downto 0);
 
-		oArready:   out std_logic;
-		iArvalid:   in  std_logic;
-		iArid:      in  std_logic_vector( 8 downto 0);
-		iAraddr:    in  std_logic_vector(38 downto 0);
-		iArlen:     in  std_logic_vector( 7 downto 0);
-		iArburst:   in  std_logic_vector( 1 downto 0);
-		iArsize:    in  std_logic_vector( 1 downto 0);
+		ARREADY:    out std_logic;
+		ARVALID:    in  std_logic;
+		ARID:       in  std_logic_vector( 8 downto 0);
+		ARADDR:     in  std_logic_vector(38 downto 0);
+		ARLEN:      in  std_logic_vector( 7 downto 0);
+		ARBURST:    in  std_logic_vector( 1 downto 0);
+		ARSIZE:     in  std_logic_vector( 1 downto 0);
 
-		iBready:    in  std_logic;
-		oBvalid:    out std_logic;
-		oBid:       out std_logic_vector( 8 downto 0);
-		oBresp:     out std_logic_vector( 1 downto 0);
+		BREADY:     in  std_logic;
+		BVALID:     out std_logic;
+		BID:        out std_logic_vector( 8 downto 0);
+		BRESP:      out std_logic_vector( 1 downto 0);
 
-		iRready:    in  std_logic;
-		oRvalid:    out std_logic;
-		oRid:       out std_logic_vector( 8 downto 0);
-		oRresp:     out std_logic_vector( 1 downto 0);
-		oRlast:     out std_logic;
-		oRdata:     out std_logic_vector(63 downto 0);
+		RREADY:     in  std_logic;
+		RVALID:     out std_logic;
+		RID:        out std_logic_vector( 8 downto 0);
+		RRESP:      out std_logic_vector( 1 downto 0);
+		RLAST:      out std_logic;
+		RDATA:      out std_logic_vector(63 downto 0);
 
 		oTraceClk:  out std_logic;
 		oTraceData: out std_logic_vector(15 downto 0)
@@ -57,46 +57,46 @@ begin
 		iRst            => wRst,
 		iClk            => iClk,
 
-		iMosi.aw.valid  => iAwvalid,
-		iMosi.aw.id     => iAwid,
-		iMosi.aw.addr   => iAwaddr,
-		iMosi.aw.len    => iAwlen,
-		iMosi.aw.burst  => iAwburst,
-		iMosi.aw.size   => iAwsize,
+		iMosi.aw.valid  => AWVALID,
+		iMosi.aw.id     => AWID,
+		iMosi.aw.addr   => AWADDR,
+		iMosi.aw.len    => AWLEN,
+		iMosi.aw.burst  => AWBURST,
+		iMosi.aw.size   => AWSIZE,
 		iMosi.aw.lock   => '0',
 		iMosi.aw.prot   => (others => '0'),
 		iMosi.aw.cache  => (others => '0'),
 		iMosi.aw.user   => (others => '0'),
 		iMosi.aw.qos    => (others => '0'),
-		iMosi.w.valid   => iWvalid,
-		iMosi.w.last    => iWlast,
-		iMosi.w.strb    => iWstrb,
-		iMosi.w.data    => iWdata,
-		iMosi.ar.valid  => iArvalid,
-		iMosi.ar.id     => iArid,
-		iMosi.ar.addr   => iAraddr,
-		iMosi.ar.len    => iArlen,
-		iMosi.ar.burst  => iArburst,
-		iMosi.ar.size   => iArsize,
+		iMosi.w.valid   => WVALID,
+		iMosi.w.last    => WLAST,
+		iMosi.w.strb    => WSTRB,
+		iMosi.w.data    => WDATA,
+		iMosi.ar.valid  => ARVALID,
+		iMosi.ar.id     => ARID,
+		iMosi.ar.addr   => ARADDR,
+		iMosi.ar.len    => ARLEN,
+		iMosi.ar.burst  => ARBURST,
+		iMosi.ar.size   => ARSIZE,
 		iMosi.ar.lock   => '0',
 		iMosi.ar.prot   => (others => '0'),
 		iMosi.ar.cache  => (others => '0'),
 		iMosi.ar.user   => (others => '0'),
 		iMosi.ar.qos    => (others => '0'),
-		iMosi.bready    => iBready,
-		iMosi.rready    => iRready,
+		iMosi.bready    => BREADY,
+		iMosi.rready    => RREADY,
 
-		oMiso.awready   => oAwready,
-		oMiso.wready    => oWready,
-		oMiso.arready   => oArready,
-		oMiso.b.valid   => oBvalid,
-		oMiso.b.id      => oBid,
-		oMiso.b.resp    => oBresp,
-		oMiso.r.valid   => oRvalid,
-		oMiso.r.id      => oRid,
-		oMiso.r.resp    => oRresp,
-		oMiso.r.last    => oRlast,
-		oMiso.r.data    => oRdata,
+		oMiso.awready   => AWREADY,
+		oMiso.wready    => WREADY,
+		oMiso.arready   => ARREADY,
+		oMiso.b.valid   => BVALID,
+		oMiso.b.id      => BID,
+		oMiso.b.resp    => BRESP,
+		oMiso.r.valid   => RVALID,
+		oMiso.r.id      => RID,
+		oMiso.r.resp    => RRESP,
+		oMiso.r.last    => RLAST,
+		oMiso.r.data    => RDATA,
 
 		oTraceClk       => oTraceClk,
 		oTraceData      => oTraceData
